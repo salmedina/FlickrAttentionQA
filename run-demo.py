647c9b7d2538f8ae580ee4ad60e383d1a800cc00
salmedina <salbed.medina@gmail.com>
2017-04-23 23:25:21 -0400

    add data wrapped request

--- run-demo.py
+++ run-demo.py
@@ -73,6 +73,13 @@ def submit():
 def ask():
     userid = request.args.get('userid')
     question = request.args.get('question')
+    if (userid is None and question is None):
+        print('Wrong request')
+        data = json.loads(request.form['data'])
+        userid = data['userid']
+        question = data['userid']
+        print(userid, question)
+
     res = qap.answer_user_question(userid, question)
     return jsonify(res)
 
